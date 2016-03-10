a409a253bcec10c2943ba0bd416507264c27dc02
Steve Zimmerman <steve.zimmerman@gmail.com>
2016-03-10 07:02:26 -0500

    off canvas work

--- app/js/application.js
+++ app/js/application.js
@@ -1,21 +1,13 @@
 $( document ).ready(function() {
 
-  $('.home #secondary-nav-container').affix({
+ /* $('.home #secondary-nav-container').affix({
     offset: {
-      top: $('#secondary-nav').offset().top,
+      top: $('#secondary-nav').offset().top + 0,
       bottom: function () {
       }
     }
   });
-  
-  $('.secondary-tech #secondary-nav-container').affix({
-    offset: {
-      top: $('#secondary-nav').offset().top,
-      bottom: function () {
-      }
-    }
-  });
-
+*/
   $( ".secondary-nav-toggle" ).on( "click", function(e) {
     e.preventDefault();
     $('html,body').animate({ scrollTop: 0 }, 'slow');
@@ -28,12 +20,8 @@ $( document ).ready(function() {
     $drawer_nav = $('#nav-drawer');
     $drawer_nav.slideToggle();
   });
-  
-  $('[data-toggle=collapse]').click(function() {
-    $('.row-offcanvas').toggleClass('active');
-  });
-  
-  
+
+  /*
   // add and remove the padding when the nav affixes
   
   $(".home #secondary-nav-container").on('affix.bs.affix', function(){
@@ -67,29 +55,97 @@ $( document ).ready(function() {
   //$("#masthead").sticky({ topSpacing: 0, className:"prime-fixed" });
   //$("#secondary-nav").sticky({ topSpacing: 0, center:true, className:"secondary-fixed" });
   //$("#tertiary-nav-container").sticky({ topSpacing:65, center:true, className:"tertiary-fixed" });
+   
+   */
   
+  var topOffset = new Array();
 
-  $('#off-canvas-nav').scotchPanel({
+  function scrollFunction(){
 
-      beforePanelOpen: function() {
-          //alert('Before Panel Opened');
-      },
-      afterPanelOpen: function() {
-          //alert('After Panel Opened');
-      },
-      beforePanelClose: function() {
-          //alert('Before Panel Closed');
-      },
-      afterPanelClose: function() {
-          //alert('After Panel Opened');
-      },
+      var scrollHeight = $(window).scrollTop();
+      var headerCounter = 0;
+      var scrolled      = 0;
+      var headerItems   = $('.header').length;
 
-      containerSelector: 'body',
-      direction: 'right',
-      clickSelector: '.toggle-panel'
+      $('.header').each(function(index, el){
+
+          var elementHeight = $(this).outerHeight();
+
+          var nextElementHeight = 0;
+          var nextElement;
+
+          if(index !== $('.header').length - 1){
+              nextElementHeight = $('.header').eq(index + 1).outerHeight();
+              nextElement       = $('.header').eq(index + 1);
+          }
+
+          if(scrollHeight >= topOffset[headerCounter]
+              && (scrollHeight < topOffset[headerCounter + 1] || headerCounter == headerItems-1)){
+
+              scrolled = 1;
+
+              if(scrollHeight >= topOffset[headerCounter + 1] - elementHeight){
+                  $(this).css({
+                      position: "fixed",
+                      top: - (scrollHeight - (topOffset[headerCounter + 1] - elementHeight))
+                  });
+                  nextElement.css({
+                      position: "fixed",
+                      top: topOffset[headerCounter + 1] - scrollHeight
+                  });
+                  $('body').css({
+                      "padding-top": elementHeight + nextElementHeight
+                  });
+                  return false;
+              }
+              else{
+                  $(this).css({
+                      position: "fixed",
+                      top: 0
+                  }).addClass('affix-top');
+                  if(nextElement) {
+                    nextElement.css({
+                        position: "static",
+                    });
+                  }
+                  $('body').css({
+                      "padding-top": elementHeight
+                  });
+              }
+
+          }
+          else{
+              $(this).css({
+                  position: "static"
+              }).removeClass('affix-top');
+          }
+
+          headerCounter++;
+      });
+
+      if(scrolled == 0){
+          $('body').css({
+              "padding-top": 0
+          });
+      }
+  }
+
+  $(function(){
+      $('.header').each(function(){
+          topOffset.push($(this).offset().top);
+      });
   });
 
+  $(window).scroll(scrollFunction);
+   // ADD SLIDEDOWN ANIMATION TO DROPDOWN //
+  $('.dropdown').on('show.bs.dropdown', function(e){
+    $(this).find('.dropdown-menu').first().stop(true, true).slideDown('fast');
+  });
 
+  // ADD SLIDEUP ANIMATION TO DROPDOWN //
+  $('.dropdown').on('hide.bs.dropdown', function(e){
+    $(this).find('.dropdown-menu').first().stop(true, true).slideUp('fast');
+  });
 });
 
 toggle_caret = function(el) {
