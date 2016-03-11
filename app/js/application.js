7d58950d8f638d24dc212f5c62999ff282be155a
Steve Zimmerman <steve.zimmerman@gmail.com>
2016-03-11 07:20:38 -0500

    headers work and search and dropdown work

--- app/js/application.js
+++ app/js/application.js
@@ -1,16 +1,8 @@
 $( document ).ready(function() {
 
- /* $('.home #secondary-nav-container').affix({
-    offset: {
-      top: $('#secondary-nav').offset().top + 0,
-      bottom: function () {
-      }
-    }
-  });
-*/
   $( ".secondary-nav-toggle" ).on( "click", function(e) {
     e.preventDefault();
-    $('html,body').animate({ scrollTop: 0 }, 'slow');
+    $('html,body').animate({ scrollTop: 0 }, 'fast');
     
     // get drawer toggle 
     $drawer_toggle_arrow = $('.drawer-indicator');
@@ -18,50 +10,81 @@ $( document ).ready(function() {
     toggle_caret($drawer_toggle_arrow);
     
     $drawer_nav = $('#nav-drawer');
-    $drawer_nav.slideToggle();
+    $drawer_nav.slideToggle('fast', function() {
+      scrollFunction();
+    });
+    
   });
 
-  /*
-  // add and remove the padding when the nav affixes
+
+
+  // bootstrap sticky headers 
   
-  $(".home #secondary-nav-container").on('affix.bs.affix', function(){
-    //$('#masthead.navbar-fixed-top').addClass('push-it');
-    $('#slot-2').addClass('add-pad');
-    $('#secondary-nav-brand img').addClass('sticky-logo');
-  });
-  
-  $(".home #secondary-nav-container").on('affix-top.bs.affix', function(){
-    //$('#masthead.navbar-fixed-top').removeClass3('push-it');
-    $('#slot-2').removeClass('add-pad');
-    $('#secondary-nav-brand img').removeClass('sticky-logo');
-  });
-  
-  $(".secondary-tech #secondary-nav-container").on('affix.bs.affix', function(){
-    $('#hero').addClass('add-pad');
-    $('#secondary-nav-brand img').addClass('sticky-logo');
-  });
-  
-  $(".secondary-tech #secondary-nav-container").on('affix-top.bs.affix', function(){
-    $('#hero').removeClass('add-pad');
-    $('#secondary-nav-brand img').removeClass('sticky-logo');
-  });
-  
-  
-  $( ".tertiary-tech-toggle" ).on( "click", function(e) {
-    toggle_caret(this);
-  });
-    
+  // $('.home #secondary-nav-container').affix({
+//     offset: {
+//       top: $('#secondary-nav').offset().top + 0,
+//       bottom: function () {
+//       }
+//     }
+//   });
+//
+//   // add and remove the padding when the nav affixes
+//
+//   $(".home #secondary-nav-container").on('affix.bs.affix', function(){
+//     //$('#masthead.navbar-fixed-top').addClass('push-it');
+//     $('#slot-2').addClass('add-pad');
+//     $('#secondary-nav-brand img').addClass('sticky-logo');
+//   });
+//
+//   $(".home #secondary-nav-container").on('affix-top.bs.affix', function(){
+//     //$('#masthead.navbar-fixed-top').removeClass3('push-it');
+//     $('#slot-2').removeClass('add-pad');
+//     $('#secondary-nav-brand img').removeClass('sticky-logo');
+//   });
+//
+//   $(".secondary-tech #secondary-nav-container").on('affix.bs.affix', function(){
+//     $('#hero').addClass('add-pad');
+//     $('#secondary-nav-brand img').addClass('sticky-logo');
+//   });
+//
+//   $(".secondary-tech #secondary-nav-container").on('affix-top.bs.affix', function(){
+//     $('#hero').removeClass('add-pad');
+//     $('#secondary-nav-brand img').removeClass('sticky-logo');
+//   });
+//
+//
+//   $( ".tertiary-tech-toggle" ).on( "click", function(e) {
+//     toggle_caret(".tertiary-tech-toggle span");
+//   });
+//
   
   //$("#masthead").sticky({ topSpacing: 0, className:"prime-fixed" });
   //$("#secondary-nav").sticky({ topSpacing: 0, center:true, className:"secondary-fixed" });
   //$("#tertiary-nav-container").sticky({ topSpacing:65, center:true, className:"tertiary-fixed" });
    
-   */
+   
   
+  /* set up the scrolltracking for animations */
+  var wow = new WOW(
+    {
+      boxClass:     'wow',      // animated element css class (default is wow)
+      animateClass: 'animated', // animation css class (default is animated)
+      offset:       0,          // distance to the element when triggering the animation (default is 0)
+      mobile:       true,       // trigger animations on mobile devices (default is true)
+      live:         true,       // act on asynchronously loaded content (default is true)
+      callback:     function(box) {
+        // the callback is fired every time an animation is started
+        // the argument that is passed in is the DOM node being animated
+      },
+      scrollContainer: null // optional scroll container selector, otherwise use window
+    }
+  );
+  wow.init();
+  
+  /* set up the sticky headers */
   var topOffset = new Array();
 
   function scrollFunction(){
-
       var scrollHeight = $(window).scrollTop();
       var headerCounter = 0;
       var scrolled      = 0;
@@ -102,11 +125,9 @@ $( document ).ready(function() {
                   $(this).css({
                       position: "fixed",
                       top: 0
-                  }).addClass('affix-top');
+                  }).removeClass('affix-top').addClass('affix');
                   if(nextElement) {
-                    nextElement.css({
-                        position: "static",
-                    });
+                    nextElement.addClass('affix-top');
                   }
                   $('body').css({
                       "padding-top": elementHeight
@@ -115,9 +136,7 @@ $( document ).ready(function() {
 
           }
           else{
-              $(this).css({
-                  position: "static"
-              }).removeClass('affix-top');
+              $(this).addClass('affix-top').removeClass('affix').css('position', '').css('top', '0');
           }
 
           headerCounter++;
@@ -137,18 +156,19 @@ $( document ).ready(function() {
   });
 
   $(window).scroll(scrollFunction);
+  
    // ADD SLIDEDOWN ANIMATION TO DROPDOWN //
-  $('.dropdown').on('show.bs.dropdown', function(e){
-    $(this).find('.dropdown-menu').first().stop(true, true).slideDown('fast');
+  $('#masthead .dropdown').on('show.bs.dropdown', function(e){
+    $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
   });
 
   // ADD SLIDEUP ANIMATION TO DROPDOWN //
-  $('.dropdown').on('hide.bs.dropdown', function(e){
-    $(this).find('.dropdown-menu').first().stop(true, true).slideUp('fast');
+  $('#masthead .dropdown').on('hide.bs.dropdown', function(e){
+    $(this).find('.dropdown-menu').first().stop(true, true).hide();
   });
 });
 
 toggle_caret = function(el) {
-  $caret = el;
+  $caret = $(el);
   $caret.toggleClass('nav-caret-down').toggleClass('nav-caret-up');
 }
